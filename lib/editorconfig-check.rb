e85c4f0ba90e936def91e995527e7f3570d325d3
Stephen <stephengroat@users.noreply.github.com>
2016-07-31 15:52:57 -0700

    Update editorconfig-check.rb

--- lib/editorconfig-check.rb
+++ lib/editorconfig-check.rb
@@ -1,1 +1,17 @@
 require "editorconfig"
+
+module EditorConfig
+  @byte_order_marks = {
+    '\xef\xbb\xbf' => 'utf-8-bom',
+    '\xfe\xff' => 'utf-16be',
+    '\xff\xfe' => 'utf-16le',
+    '\x00\x00\xfe\xff' => 'utf-32be',
+    '\xff\xfe\x00\x00' => 'utf-32le',
+  }
+
+  @line_endings = {
+    'crlf' => '\r\n',
+    'lf' =>'\n',
+    'cr' => '\r',
+  }
+end

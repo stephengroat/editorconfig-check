70f0d3d4bc636a544ca6b728fb6e36ed489040a4
Stephen <stephengroat@users.noreply.github.com>
2016-08-27 15:34:26 -0700

    Update editorconfig-check.rb

--- lib/editorconfig-check.rb
+++ lib/editorconfig-check.rb
@@ -7,39 +7,31 @@ module EditorConfig
     '\xff\xfe' => 'utf-16le',
     '\x00\x00\xfe\xff' => 'utf-32be',
     '\xff\xfe\x00\x00' => 'utf-32le'
-  }
+  }.freeze
 
   LINE_ENDINGS = {
     'crlf' => '\r\n',
     'lf' => '\n',
     'cr' => '\r'
-  }
+  }.freeze
 
   def check(file)
     line_num = 0
     text = File.open(file).read
     text.each_line do |line|
       check_indentation(line, 'space')
-      #rescue
-      #  next
+      # TODO: does this need a rescue/next clause
     end
   end
 
   def check_indentation(line, indent_style, indent_size)
     indent = line[/^\s*/]
-    if indent_style == 'space'
-      if indent =~ /\t/
-        raise 'Tab indentation found'
-      elsif (indent.count(' ') % indent_size).nonzero?
-        raise 'Incorrect space indent size found'
-      end
-    elsif indent_style == 'tab'
-      if indent =~ /\s/
-        raise 'Space indentation found'
-      elsif (indent.count('\t') % indent_size).nonzero?
-        raise 'Incorrect tab indent size found'
-      end
-    end
+    # Space indentation
+    raise 'Tab indentation found' if indent_style == 'space' && indent =~ /\t/
+    raise 'Incorrect spxce indent size found' if indent_style == 'space' && (indent.count(' ') % indent_size).nonzero?
+    # Tab Indentation
+    raise 'Space indentation found' if indent_style == 'tab' && indent =~ /\s/
+    raise 'Incorrect tab indent size found' if indent_style == 'tab' && (indent.count('\t') % indent_size).nonzero?
   end
 
   def check_trailing_whitespace(line)

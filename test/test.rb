f6288f42436b0e0853146a0434e13d8e5fe1837d
Stephen <stephengroat@users.noreply.github.com>
2016-10-19 19:47:42 -0700

    Update test.rb

--- test/test.rb
+++ test/test.rb
@@ -1,2 +1,12 @@
-class Test
+require 'coveralls'
+require_relative '../lib/editorconfig_check'
+require "test/unit"
+
+Coveralls.wear!
+
+class Test < Test::Unit::TestCase
+  def test_check_trailing_whitespace
+    assert_raise(EditorConfig.check_trailing_whitespace(" "))
+    assert_nil(EditorConfig.check_trailing_whitespace(""))
+  end
 end

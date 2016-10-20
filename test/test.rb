4b0f51178e7eb07009a3189e66b0f63e8d495415
Stephen <stephengroat@users.noreply.github.com>
2016-10-19 20:12:58 -0700

    Update test.rb

--- test/test.rb
+++ test/test.rb
@@ -1,12 +1,12 @@
 require 'coveralls'
 require 'editorconfig-check'
-require "test/unit"
+require 'minitest/autorun'
 
 Coveralls.wear!
 
-class Test < Test::Unit::TestCase
+class TestEditorConfigCheck < Minitest::Test
   def test_check_trailing_whitespace
-    assert_raise(EditorConfig.check_trailing_whitespace(" "))
+    assert_raises(EditorConfig.check_trailing_whitespace(" "))
     assert_nil(EditorConfig.check_trailing_whitespace(""))
   end
 end

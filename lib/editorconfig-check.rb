1da2b39ad2bc50c7692cb8449d8c4731e72b0126
Stephen <stephengroat@users.noreply.github.com>
2017-02-05 20:00:58 -0800

    Update editorconfig-check.rb

--- lib/editorconfig-check.rb
+++ lib/editorconfig-check.rb
@@ -1,7 +1,7 @@
 require 'editorconfig'
 
 module EditorConfigCheck
-  BYTE_ORDER_MARKS = {
+  @BYTE_ORDER_MARKS = {
     '\xef\xbb\xbf' => 'utf-8-bom',
     '\xfe\xff' => 'utf-16be',
     '\xff\xfe' => 'utf-16le',
@@ -9,10 +9,15 @@ module EditorConfigCheck
     '\xff\xfe\x00\x00' => 'utf-32le'
   }.freeze
 
-  LINE_ENDINGS = {
+  @LINE_ENDINGS = {
     'crlf' => '\r\n',
     'lf' => '\n',
     'cr' => '\r'
+  }.freeze
+  
+  @INDENT = {
+    'tab' => '\t',
+    'space' => ' '
   }.freeze
 
   def check(file)
@@ -21,6 +26,7 @@ module EditorConfigCheck
     text.each_line do |line|
       check_indentation(line, 'space')
       check_trailing_whitespace(line)
+      check_end_of_line(line, 'lf')
       line_num += 1
       # TODO: does this need a rescue/next clause
     end
@@ -28,23 +34,15 @@ module EditorConfigCheck
 
   def check_indentation(line, indent_style, indent_size)
     indent = line[/^\s*/]
-    # Space indentation
-    if indent_style == 'space'
-      raise 'Tab indentation found' if indent =~ /\t/
-      raise 'Incorrect spxce indent size found' if (indent.count(' ') % indent_size).nonzero?
-    # Tab Indentation
-    elsif indent_style == 'tab'
-      raise 'Space indentation found' if indent =~ / /
-      raise 'Incorrect tab indent size found' if (indent.count('\t') % indent_size).nonzero?
-    end
+    raise "Non #{indent_style} indentation found" if /(?!#{@IDENT[indent_style]})/ =~ indent 
+    raise 'Incorrect indent size found' if (indent.count(@INDENT[indent_style]) % indent_size).nonzero?
   end
 
   def check_trailing_whitespace(line)
-    trailing_whitespace = line[/[\s]+$/]
-    raise 'Trailing whitespace found' unless trailing_whitespace.nil?
+    raise 'Trailing whitespace found' unless line[/[\s]+$/].nil?
   end
   
   def check_end_of_line(line, end_of_line)
-    raise "Incorrect end of line found" if /LINE_ENDINGS[end_of_line]/ =~ line
+    raise "Incorrect end of line found" if /#{@LINE_ENDINGS[end_of_line]}/ =~ line
   end
 end

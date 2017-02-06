4488561a5511ef6f6539e3a2c82c3d8774c4e6af
Stephen <stephengroat@users.noreply.github.com>
2017-02-05 21:43:01 -0800

    Update editorconfig_check.rb

--- lib/editorconfig_check.rb
+++ lib/editorconfig_check.rb
@@ -37,7 +37,7 @@ module EditorConfigCheck
     indent = line[/^[\t ]+/]
     return if indent.nil?
     raise "Non #{indent_type} indentation found"\
-      if !indent[/(?!#{@whitepace[indent_type]})/].nil?
+      unless indent[/(?!#{@whitepace[indent_type]})/].nil?
     raise 'Incorrect indent size found'\
           if (indent.count(@whitespace[indent_type]) % indent_size).nonzero?
   end

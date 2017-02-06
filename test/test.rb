b5817dd9e1f8e37c29efd56dee10c60e2c993ff8
Stephen <stephengroat@users.noreply.github.com>
2017-02-05 20:44:11 -0800

    Update test.rb

--- test/test.rb
+++ test/test.rb
@@ -6,7 +6,7 @@ require 'minitest/autorun'
 class TestEditorConfigCheck < Minitest::Test
   def test_check_trailing_whitespace
     exception = assert_raises(RuntimeError)\
-       {EditorConfigCheck.check_trailing_whitespace(' ') }
+       { EditorConfigCheck.check_trailing_whitespace(' ') }
     assert_equal('Trailing whitespace found', exception.message)
   end
 end

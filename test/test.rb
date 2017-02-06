ced5352c5b3d0a7b5d87359da0a3d8d728eb8bda
Stephen <stephengroat@users.noreply.github.com>
2017-02-05 20:47:34 -0800

    Update test.rb

--- test/test.rb
+++ test/test.rb
@@ -9,4 +9,9 @@ class TestEditorConfigCheck < Minitest::Test
        { EditorConfigCheck.check_trailing_whitespace(' ') }
     assert_equal('Trailing whitespace found', exception.message)
   end
+  def test_check_indentation
+    exception = assert_raises(RuntimeError)\
+       { EditorConfigCheck.check_indentation(' s', 'tab', 1) }
+    assert_equal('Non tab indentation found', exception.message)
+  end
 end
